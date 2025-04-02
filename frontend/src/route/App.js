12c8c735004140ebeb6c740856437a9d542104f5
HakanaSanae <daniel141317.work@gmail.com>
2025-04-02 23:57:44 +0800

    feat: routes for dashboard sections

--- frontend/src/route/App.js
+++ frontend/src/route/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState}from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
-import HomePage from '../view/HomePage.js';
+import MainPage from '../view/MainPage.js';
 import Layout from '../view/Layout.js';
 import { Routes, Route } from "react-router-dom";
 import LoginPage from '../view/LoginPage.js';
 import RegisterPage from '../view/RegisterPage.js';
 import ESGDashBoard from '../component/ESGDashBoard.js';
+import HomePage from '../view/HomePage.js';
 
 function App() {
     const acc = localStorage.getItem('account');
@@ -15,9 +16,15 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout account = {account} setAccount = {setAccount}/>} >
-                <Route index element={<HomePage />} />
+                <Route index element={<MainPage /> } />
                 <Route path="login" element={<LoginPage account = {account} setAccount = {setAccount}/>} />
-                <Route path="dashboard" element={<ESGDashBoard account = {account}/>} />
+                <Route path="dashboard" element={<HomePage /> }>
+                    <Route path="Overall-ESG-Performance" element={<ESGDashBoard account = {account} setAccount = {setAccount}/>} />
+                    <Route path="Sector-based-Comparison" element={<h1>Sector based comparison</h1>} />
+                    <Route path="Top-Bottom-Performers" element={<h1>Top bottom performers</h1>} />
+                    <Route path="ESG-Score-Breakdown" element={<h1>ESG Score breakdown</h1>} />
+                    <Route path="Sector-Industry-Leaders" element={<h1>ESG Sector industry leaders</h1>} />
+                </Route>
                 <Route path="register" element={<RegisterPage account = {account} setAccount = {setAccount}/>} />
                 <Route path="*" element={<h1>404 Not Found</h1>} />
             </Route>
