e48bef48ed825f468ae3cfb33a0ab72c8ce782d1
HakanaSanae <daniel141317.work@gmail.com>
2025-04-06 16:47:32 +0800

    feat: added simple response when uploading pdf

--- backend/app/Http/Controllers/fileUploadController.php
+++ backend/app/Http/Controllers/fileUploadController.php
@@ -1,16 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
-class fileUploadController {
+class fileUploadController
+{
     public function riskAnalysis(Request $request)
     {
-        $file = $request->file('file')->store('risk-analysis');
+        //TODO: send pdf to Andrew's API
+        $file = $request->file('file')->store('risk-analysis', 'public');
+
+
+        $data = [
+            "Total_Risk_Level"=> 1,
+            "E" => [
+                "Risk_Level" => 1,
+                "Explanation" => "The company did a great job in reducing carbon emission . ",
+                "Suggestion" => "The company can focus more on energy management . ",
+            ],
+            'S' => [
+                "Risk_Level" => 1,
+                "Explanation" => "The company changed policy to promote labor rights . ",
+                "Suggestion" => "Focus more on DEI.",
+            ],
+            'G' => [
+                "Risk_Level" => 1,
+                "Explanation" => "The Board of Directors solve the conflict effectively in this year.",
+                "Suggestion" => "As all members in the Board are male, they company and appoint and add more female talents to the Board . "
+            ]
+        ];
 
         return response()->json([
             'success' => true,
-            'message' => 'File uploaded successfully.',
+            'message' => $data,
         ]);
     }
 }
