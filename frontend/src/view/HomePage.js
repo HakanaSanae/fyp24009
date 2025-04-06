b19e455739b5d2b2d7245a86a88977c8fb571311
HakanaSanae <daniel141317.work@gmail.com>
2025-04-06 16:53:16 +0800

    feat: added reset button for risk analysis page, changed layout

--- frontend/src/view/HomePage.js
+++ frontend/src/view/HomePage.js
@@ -4,64 +4,77 @@ import { useNavigate } from 'react-router-dom';
 function HomePage() {
     return (
         <div className="home-page">
-            <h1>
+            <h1 style={{ textAlign: "center"}}>
                 ESG Dashboards
             </h1>
-            <div class="home-buttons-container">
-                <h4>
-                    Overall ESG Performance
-                </h4>
-                <a href="/dashboard/Overall-ESG-Performance" >
-                    <button variant="primary">
-                        View Here
-                    </button>
-                </a>
-            </div>
+            <div className="home-page-item-container">
+                <div class="home-buttons-container">
+                    <h4>
+                        Overall ESG Performance
+                    </h4>
+                    <a href="/dashboard/Overall-ESG-Performance" >
+                        <button variant="primary">
+                            View Here
+                        </button>
+                    </a>
+                </div>
 
-            <div class="home-buttons-container">
-                <h4>
-                    Sector-based Comparison
-                </h4>
-                <a href="/dashboard/Sector-based-Comparison">
-                    <button variant="primary">
-                        View Here
-                    </button>
-                </a>
-            </div>
+                <div class="home-buttons-container">
+                    <h4>
+                        Sector-based Comparison
+                    </h4>
+                    <a href="/dashboard/Sector-based-Comparison">
+                        <button variant="primary">
+                            View Here
+                        </button>
+                    </a>
+                </div>
 
-            <div class="home-buttons-container">
-                <h4>
-                    Top & Bottom Performers
-                </h4>
-                <a href="/dashboard/Top-Bottom-Performers">
-                    <button variant="primary">
-                        View Here
-                    </button>
-                </a>
-            </div>
+                <div class="home-buttons-container">
+                    <h4>
+                        Top & Bottom Performers
+                    </h4>
+                    <a href="/dashboard/Top-Bottom-Performers">
+                        <button variant="primary">
+                            View Here
+                        </button>
+                    </a>
+                </div>
 
-            <div class="home-buttons-container">
-                <h4>
-                    ESG Score Breakdown
-                </h4>
-                <a href="/dashboard/ESG-Score-Breakdown">
-                    <button variant="primary">
-                        View Here
-                    </button>
-                </a>
-            </div>
+                <div class="home-buttons-container">
+                    <h4>
+                        ESG Score Breakdown
+                    </h4>
+                    <a href="/dashboard/ESG-Score-Breakdown">
+                        <button variant="primary">
+                            View Here
+                        </button>
+                    </a>
+                </div>
 
-            <div class="home-buttons-container">
-                <h4>
-                    Sector & Industry Leaders
-                </h4>
-                <a href="/dashboard/Sector-Industry-Leaders">
-                    <button variant="primary" class="home-buttons">
-                        View Here
-                    </button>
-                </a>
-            </div>
+                <div class="home-buttons-container">
+                    <h4>
+                        Sector & Industry Leaders
+                    </h4>
+                    <a href="/dashboard/Sector-Industry-Leaders">
+                        <button variant="primary" class="home-buttons">
+                            View Here
+                        </button>
+                    </a>
+                </div>
 
+                <div class="home-buttons-container">
+                    <h4>
+                        ESG Risk Analysis
+                    </h4>
+                    <a href="/dashboard/ESG-Risk-Analysis">
+                        <button variant="primary">
+                            View Here
+                        </button>
+                    </a>
+                </div>
+
+            </div>
         </div>
     )
 }
--- frontend/src/view/RiskAnalysisPage.js
+++ frontend/src/view/RiskAnalysisPage.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import apiClient from '../interface/apiClient';
+import RiskAnalysisInformation from '../component/RiskAnalysisInformation';
 
 function RiskAnalysisPage() {
 
     const [file, setFile] = useState(null);
+    const [E_data, setEData] = useState(null);
+    const [S_data, setSData] = useState(null);  
+    const [G_data, setGData] = useState(null);
+    const [risk_level, setRiskLevel] = useState(null);
+    const fileInputRef = useRef(null);
+    const [resetSubmission, setResetSubmission] = useState(false); 
 
     const handleFileUpload = (event) => {
         setFile(event.target.files[0]);
+        setResetSubmission(true); 
     };
+
+    const reset = () => {
+        setFile(null);
+        setEData(null);
+        setSData(null);
+        setGData(null);
+        setRiskLevel(null);
+        fileInputRef.current.value = null;
+        setResetSubmission(false);
+    }; 
 
     const submitFile = async () => {
         const formData = new FormData();
@@ -19,6 +37,14 @@ function RiskAnalysisPage() {
 
             if (response.success) {
                 console.log("File uploaded successfully");
+                fileInputRef.current.value = null;
+                const data = response.message;
+                setRiskLevel(data.Total_Risk_Level);
+                setEData(data.E);
+                setSData(data.S);
+                setGData(data.G);
+                setFile(null); 
+                setResetSubmission(true);
             } else {
                 console.error("File upload failed");
             }
@@ -29,18 +55,51 @@ function RiskAnalysisPage() {
 
     return (
         <div className = "risk-analysis-page">
+            
+            {
+                risk_level && (
+                    <div className="risk-level" style={{justifySelf: "center"}}>
+                        <h1>Total Risk Level: {risk_level}</h1>
+                    </div>
+                )
+            }
+            {
+                E_data && (
+                    <RiskAnalysisInformation type="Environmental" data={E_data} />
+                )
+            }
+            {
+                S_data && (
+                    <RiskAnalysisInformation type="Social" data={S_data} />
+                )   
+            }
+            {
+                G_data && (
+                    <RiskAnalysisInformation type="Governance" data={G_data} />
+                )
+            }
+
             <input
                 type="file"
                 accept=".pdf"
                 onChange={handleFileUpload}
+                ref = {fileInputRef}
             />
 
-            {file && (
-                <div>
-                    <h2>File Uploaded: {file.name} </h2>
+            <div style={{ display: 'flex', justifyContent: 'space-between' , marginBottom: "3vh"}}>
+
+                {resetSubmission && (
+                    <button onClick={reset}>Reset</button>
+                )}
+
+                {file && (
                     <button onClick={submitFile}>Submit File</button>
-                </div>
-            )}
+                )}
+
+                
+            </div>
+            
+
         </div>
     )
 }
