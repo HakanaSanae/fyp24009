1b0610708f118e9b50a8c8e6fa501ba445a033d2
HakanaSanae <daniel141317.work@gmail.com>
2025-04-08 14:11:44 +0800

    chore: change apiClient from class component to functional component

--- frontend/src/interface/apiClient.js
+++ frontend/src/interface/apiClient.js
@@ -1,80 +1,76 @@
 import axios from 'axios';
 
-class apiClient {
+const testing = true; 
+const backendPath = testing ? 'http://localhost:8000/api' : 'http://fyp-laravel-server2-env.eba-3gaqk4pn.ap-northeast-3.elasticbeanstalk.com/api'; 
+// axios.defaults.withCredentials = true; 
+
+const handleError = (response, navigate, error = 'Internal Server Error') =>{
     
-    constructor() {
-        this.testing = true; 
-        this.backendPath = this.testing ? 'http://localhost:8000/api' : 'http://fyp-laravel-server2-env.eba-3gaqk4pn.ap-northeast-3.elasticbeanstalk.com/api'; 
-        // axios.defaults.withCredentials = true; 
+    if (!response) {
+        throw new Error('Network error, please check your connection');
     }
 
-    async login(formData) {
-        try {
-            const response = await axios.post(`${this.backendPath}/login`, formData); 
-            return response.data;
-        } catch (error) {
-            return this.handleError(error.response, 'Login failed'); 
-        }
-    }
-
-    async register(formData) {
-        try{
-            const response = await axios.post(`${this.backendPath}/register`, formData); 
-            return response.data;
-        } catch (error) {
-            return this.handleError(error.response, 'Register failed'); 
-        }
-    }
-
-    async logout() {
-        try{
-            const response = await axios.get(`${this.backendPath}/logout`); 
-            return response.data;
-        } catch (error) {
-            return this.handleError(error.response, 'Logout failed'); 
-        }
-    }
-
-    async submitRiskAnalysisFile(formData) {
-        try {
-            const response = await axios.post(`${this.backendPath}/risk-analysis`, formData); 
-            return response.data;
-        } catch (error) {
-            return this.handleError(error.response, 'File upload failed'); 
-        }
-    }
-
-    async fetchUserInfo() {
-        try{
-            const response = await axios.get(`${this.backendPath}/user-info`); 
-            return response.data; 
-        } catch (error) {
-            return this.handleError(error.response, 'Fetch user info failed'); 
-        }
-    }
-
-    async updateUserInfo(formData) {
-        try{
-            const response = await axios.put(`${this.backendPath}/update-user-info`, formData); 
-            return response.data;
-        } catch (error) {
-            return this.handleError(error.response, 'Update user info failed'); 
-        }
-    }
-
-    //private methods 
-    handleError(response, error = 'Internal Server Error'){
-        if (!response) {
-            throw new Error('Network error, please check your connection');
-        }
-
-        if ( response.status == 401 ) {
-            window.location.href = '/login';
-            throw new Error('Unauthorized access, please login again');
-        } else {
-            throw new Error(error); 
-        }
+    if ( response.status == 401 ) {
+        localStorage.removeItem('account');
+        navigate('/login');
+        throw new Error('Unauthorized access, please login again');
+    } else {
+        throw new Error(error); 
     }
 }
 
-export default new apiClient();
+export const login = async (formData, navigate) => {
+    try {
+        const response = await axios.post(`${backendPath}/login`, formData); 
+        return response.data;
+    } catch (error) {
+        return handleError(error.response, navigate, 'Login failed'); 
+    }
+}
+
+export const register = async (formData, navigate) => {
+    try{
+        const response = await axios.post(`${backendPath}/register`, formData); 
+        return response.data;
+    } catch (error) {
+        return handleError(error.response, navigate, 'Register failed'); 
+    }
+}
+
+
+export const logout = async (navigate) => {
+    try{
+        const response = await axios.get(`${backendPath}/logout`); 
+        navigate('/login');
+        return response.data;
+    } catch (error) {
+        return handleError(error.response, navigate, 'Logout failed'); 
+    }
+}
+
+export const submitRiskAnalysisFile = async (formData, navigate) => {
+    try {
+        const response = await axios.post(`${backendPath}/risk-analysis`, formData); 
+        return response.data;
+    } catch (error) {
+        return handleError(error.response, navigate, 'File upload failed'); 
+    }
+}
+
+export const fetchUserInfo = async (navigate) => {
+    try{
+        const response = await axios.get(`${backendPath}/user-info`); 
+        return response.data; 
+    } catch (error) {
+        return handleError(error.response, navigate, 'Fetch user info failed'); 
+    }
+}
+
+export const updateUserInfo = async (formData, navigate) => {
+    try{
+        const response = await axios.put(`${backendPath}/update-user-info`, formData); 
+        return response.data;
+    } catch (error) {
+        return handleError(error.response, navigate, 'Update user info failed'); 
+    }
+}
