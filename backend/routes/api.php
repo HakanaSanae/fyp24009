d0e6831b751ccbb3d61da97816b10c8c22e224e6
HakanaSanae <daniel141317.work@gmail.com>
2025-04-08 16:24:28 +0800

    feat: add auth middleware to protect routes that can only be used after login.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AccountController; 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\fileUploadController;
 
 $router = app()->get('router');
@@ -13,5 +13,14 @@ Route::get('/user', function (Request $request) {
 
 $router->post('/login', [AccountController::class, 'login']);
 $router->post('/register', [AccountController::class, 'register']);
-$router->get('/logout', [AccountController::class, 'logout']);
-$router->post('/risk-analysis', [fileUploadController::class, 'riskAnalysis']);
+
+$router->middleware(['auth']) -> group(
+    function ( $router ) {
+        $router->get('/logout', [AccountController::class, 'logout']);
+        $router->post('/risk-analysis', [fileUploadController::class, 'riskAnalysis']);
+        $router->get('/user-info', [AccountController::class, 'getUserInfo']);
+        $router->put('/update-user-info', [AccountController::class, 'updateUserInfo']);
+
+    }
+);
+
