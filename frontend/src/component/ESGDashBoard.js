e703f38c4633cd8b12c58e638b82902e28b42052
HakanaSanae <daniel141317.work@gmail.com>
2025-04-09 01:35:10 +0800

    feat: assign correct tableau path based on the path

--- frontend/src/component/ESGDashBoard.js
+++ frontend/src/component/ESGDashBoard.js
@@ -1,18 +1,25 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-function ESGDashBoard ({ account }) {
+function ESGDashBoard () {
 
-    const loginStatus = () => {
-        return (
-            account ? true : false
-        ) 
+    const location = useLocation();
+    const pathName = location.pathname.split('/')[2]; // Get the path name from the URL
+
+    const path = {
+        'Overall-ESG-Performance' : '', 
+        'YOY-ESG-Change': 'shared/J9HFCZPMS', 
+        'Sector-Wise-ESG-Performance': 'shared/8623594D7', 
+        'ESG-Score-Breakdown' : 'shared/SKDJ4QCB2', 
+        'ESG-Score-Distribution': 'shared/R62RSMP7K', 
+        'Custom-Comparison' : 'shared/4393JMNFQ'
     }
 
+    const componentPath = path[pathName]; 
+    console.log(componentPath); // Log the component path to the console
+
     useEffect(() => {
-        if (!loginStatus()) {
-            return; 
-        }
-        var divElement = document.getElementById('viz1736230714700');                    
+        var divElement = document.getElementById('viz1744115478583');                    
         var vizElement = divElement.getElementsByTagName('object')[0];                    
         vizElement.style.width='1016px';
         vizElement.style.height='991px';                    
@@ -22,34 +29,32 @@ function ESGDashBoard ({ account }) {
     }, [])
 
     return(
-        loginStatus() ? 
-        (
-            <div className='tableauPlaceholder' id='viz1736230714700' style={{position: 'relative', margin: '30px 0px'}}>
-                <noscript>
-                    <a href='#'>
-                        <img alt='ESGenius ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ES&#47;ESG_17362298565400&#47;Story1&#47;1_rss.png' style='border: none' />
-                    </a>
-                </noscript>
-                <object className='tableauViz'  style={{display: 'none'}}>
-                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
-                    <param name='embed_code_version' value='3' /> 
-                    <param name='site_root' value='' />
-                    <param name='name' value='ESG_17362298565400&#47;Story1' />
-                    <param name='tabs' value='no' />
-                    <param name='toolbar' value='yes' />
-                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ES&#47;ESG_17362298565400&#47;Story1&#47;1.png' /> 
-                    <param name='animate_transition' value='yes' />
-                    <param name='display_static_image' value='yes' />
-                    <param name='display_spinner' value='yes' />
-                    <param name='display_overlay' value='yes' />
-                    <param name='display_count' value='yes' />
-                    <param name='language' value='en-GB' />
-                </object>
-            </div>
-        ) : (
-            <h1> Please Login to access the dashboard </h1>
-        )
+        <div className='tableauPlaceholder' id='viz1744115478583' style={{position: 'relative', margin: '30px 0px'}}>
+            <noscript>
+            <a href='#'>
+                <img alt='ESGenius Data Dashboard ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ES&#47;ESG_final&#47;Story1&#47;1_rss.png' style={{border: 'none'}}/>
+            </a>
+            </noscript>
+            <object className='tableauViz'  style={{display: 'none'}}>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                <param name='embed_code_version' value='3' /> 
+                <param name='site_root' value='' />
+                <param name='name' value='ESG_final&#47;Story1' />
+                <param name='path' value={componentPath}/> 
+                <param name='tabs' value='no' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ES&#47;ESG_final&#47;Story1&#47;1.png' /> 
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='zh-TW' />
+            </object>
+        </div>
     )
+
+   
 }
 
 export default ESGDashBoard;
