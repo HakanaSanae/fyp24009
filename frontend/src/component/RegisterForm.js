e57969ed430e5fc363576476c136819e9b936bac
HakanaSanae <daniel141317.work@gmail.com>
2025-04-09 01:36:32 +0800

    chore: formatting, minor display changes, fix: display of error messages

--- frontend/src/component/RegisterForm.js
+++ frontend/src/component/RegisterForm.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function RegistrationForm() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    username: '', 
+    username: '',
     email: '',
     password: '',
     confirmPassword: '',
@@ -24,9 +24,9 @@ function RegistrationForm() {
   const validate = () => {
     const newErrors = {};
 
-    if(!formData.username) {
+    if (!formData.username) {
       newErrors.username = 'Name cannot be empty.';
-    } 
+    }
 
     if (!formData.email) {
       newErrors.email = 'Email address cannot be empty.';
@@ -36,14 +36,13 @@ function RegistrationForm() {
       }
     }
 
-    // Password validation
     if (formData.password !== formData.confirmPassword) {
       newErrors.confirmPassword = 'Passwords do not match.';
     } else if (formData.password.length < 8 || formData.password.length > 16 || !/\d/.test(formData.password) || !/[a-zA-Z]/.test(formData.password)) {
-      newErrors.password = 'Password must be 8 to 16 characters long and contain both letters and numbers.';
+      newErrors.password = 'Password must be: <ul><li> 8 to 16 characters long </li><li> Contains both letters and numbers.</li></ul>';
     }
 
-    if (!formData.type) {
+    if (formData.type === '') {
       newErrors.type = 'Please select an account type.';
     }
 
@@ -54,23 +53,23 @@ function RegistrationForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     if (validate()) {
       try {
         const data = await register({
-            username: formData.username,
-            email: formData.email,
-            password: formData.password,
-            type: formData.type
-        }, navigate); 
+          username: formData.username,
+          email: formData.email,
+          password: formData.password,
+          type: formData.type
+        }, navigate);
         if (data.success) {
           localStorage.setItem('account', data.name);
           navigate('/dashboard');
         } else {
-            console.error('Registration failed:', data.message);
+          console.error('Registration failed:', data.message);
         }
       } catch (error) {
-          console.error('Error:', error);
+        console.error('Error:', error);
       }
     }
   };
@@ -78,8 +77,8 @@ function RegistrationForm() {
   return (
     <div>
       <form onSubmit={handleSubmit} autoComplete="off">
-        <div className = "form-item">
-          <div className = "form-input-error-container">
+        <div className="form-item">
+          <div className="form-input-error-container">
             <input
               type="username"
               name="username"
@@ -89,11 +88,10 @@ function RegistrationForm() {
             />
             {errors.username && <div style={{ color: 'red' }}>{errors.username}</div>}
           </div>
-          
         </div>
 
-        <div className = "form-item">
-          <div className = "form-input-error-container">
+        <div className="form-item">
+          <div className="form-input-error-container">
             <input
               type="email"
               name="email"
@@ -103,11 +101,11 @@ function RegistrationForm() {
             />
             {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
           </div>
-          
+
         </div>
 
-        <div className = "form-item">
-          <div className = "form-input-error-container">
+        <div className="form-item">
+          <div className="form-input-error-container">
             <input
               type="password"
               name="password"
@@ -116,39 +114,42 @@ function RegistrationForm() {
               placeholder='Password'
               autoComplete="new-password"
             />
-            {errors.password && <div style={{ color: 'red' }}>{errors.password}</div>}
+            {errors.password && <div style={{ color: 'red' }} dangerouslySetInnerHTML={{ __html: errors.password }}></div>}
           </div>
-          
+
         </div>
 
-        <div className = "form-item">
-          <div className = "form-input-error-container">
+        <div className="form-item">
+          <div className="form-input-error-container">
             <input
               type="password"
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleChange}
               placeholder='Confirm Password'
-              
+
             />
             {errors.confirmPassword && <div style={{ color: 'red' }}>{errors.confirmPassword}</div>}
           </div>
-          
+
         </div>
 
-        <div className = "form-item">
-          <select
-            name="type"
-            value={formData.type}
-            onChange={handleChange}
-          >
+        <div className="form-item">
+          <div className="form-input-error-container">
+            <select
+              name="type"
+              value={formData.type}
+              onChange={handleChange}
+            >
               <option value="">Select Account Type</option>
               <option value="company">Company</option>
               <option value="individual">Indivdual</option>
-          </select>
+            </select>
+            {errors.type && <div style={{ color: 'red' }}>{errors.type}</div>}
+          </div>
         </div>
 
-        <div className = "form-item">
+        <div className="form-item">
           <button type="submit">Register</button>
         </div>
 
@@ -158,7 +159,7 @@ function RegistrationForm() {
 
       </form>
     </div>
-    
+
   );
 }
 
