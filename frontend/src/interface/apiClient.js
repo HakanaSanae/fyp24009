8272bfdf042f0bfe4f403fa7311bb567c0e4ff26
HakanaSanae <daniel141317.work@gmail.com>
2025-04-15 17:02:31 +0800

    feat: added getMe to check for authentication for actions only available after logged in

--- frontend/src/interface/apiClient.js
+++ frontend/src/interface/apiClient.js
@@ -19,6 +19,31 @@ const handleError = (response, navigate, error = 'Internal Server Error') =>{
     }
 }
 
+const getMe = async (navigate) => {
+    try {
+        const response = await axios.get(`${backendPath}/me`); 
+        if (response.status !== 200) {
+            localStorage.removeItem('account');
+            navigate('/login');
+            throw new Error('Failed to fetch user info'); 
+        }
+
+        console.log(response.data);
+
+        if (!response.data.success) {
+            localStorage.removeItem('account');
+            navigate('/login');
+            throw new Error(response.data.message); 
+        } else {
+            if (localStorage.getItem('account') !== response.data.message.user_info.name) {
+                localStorage.setItem('account');
+            }
+        }
+    } catch (error) {
+        throw new Error(error); 
+    }
+}
+
 export const login = async (formData, navigate) => {
     try {
         const response = await axios.post(`${backendPath}/login`, formData); 
@@ -47,9 +72,10 @@ export const logout = async (navigate) => {
     }
 }
 
-export const submitRiskAnalysisFile = async (formData, navigate) => {
+export const submitPerformanceAnalysisFile = async (formData, navigate) => {
     try {
-        const response = await axios.post(`${backendPath}/risk-analysis`, formData); 
+        await getMe(navigate); 
+        const response = await axios.post(`${backendPath}/performance-analysis`, formData); 
         return response.data;
     } catch (error) {
         return handleError(error.response, navigate, 'File upload failed'); 
@@ -58,6 +84,7 @@ export const submitRiskAnalysisFile = async (formData, navigate) => {
 
 export const fetchUserInfo = async (navigate) => {
     try{
+        await getMe(navigate); 
         const response = await axios.get(`${backendPath}/user-info`); 
         return response.data; 
     } catch (error) {
@@ -67,6 +94,7 @@ export const fetchUserInfo = async (navigate) => {
 
 export const updateUserInfo = async (formData, navigate) => {
     try{
+        await getMe(navigate); 
         const response = await axios.put(`${backendPath}/update-user-info`, formData); 
         return response.data;
     } catch (error) {
