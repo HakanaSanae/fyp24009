23a8e1bb6c34940168460c70717eed81c41ab9a4
HakanaSanae <daniel141317.work@gmail.com>
2025-04-15 17:04:25 +0800

    chore: rename risk to performance

--- backend/app/Http/Controllers/fileUploadController.php
+++ backend/app/Http/Controllers/fileUploadController.php
@@ -6,26 +6,25 @@ use Illuminate\Http\Request;
 
 class fileUploadController
 {
-    public function riskAnalysis(Request $request)
+    public function performanceAnalysis(Request $request)
     {
         //TODO: send pdf to Andrew's API
-        $file = $request->file('file')->store('risk-analysis', 'public');
-
+        $file = $request->file('file')->store('performance-analysis', 'public');
 
         $data = [
-            "Total_Risk_Level"=> 1,
+            "Total_Performance_Level"=> 1,
             "E" => [
-                "Risk_Level" => 1,
+                "Performance_Level" => 1,
                 "Explanation" => "The company did a great job in reducing carbon emission . ",
                 "Suggestion" => "The company can focus more on energy management . ",
             ],
             'S' => [
-                "Risk_Level" => 1,
+                "Performance_Level" => 1,
                 "Explanation" => "The company changed policy to promote labor rights . ",
                 "Suggestion" => "Focus more on DEI.",
             ],
             'G' => [
-                "Risk_Level" => 1,
+                "Performance_Level" => 1,
                 "Explanation" => "The Board of Directors solve the conflict effectively in this year.",
                 "Suggestion" => "As all members in the Board are male, they company and appoint and add more female talents to the Board . "
             ]
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,14 +9,14 @@ $router = app()->get('router');
 
 $router->post('/login', [AccountController::class, 'login']);
 $router->post('/register', [AccountController::class, 'register']);
+$router->get('/me', [AccountController::class, 'getMe']);
 
 $router->middleware(['auth']) -> group(
     function ( $router ) {
         $router->get('/logout', [AccountController::class, 'logout']);
-        $router->post('/risk-analysis', [fileUploadController::class, 'riskAnalysis']);
+        $router->post('/performance-analysis', [fileUploadController::class, 'performanceAnalysis']);
         $router->get('/user-info', [AccountController::class, 'fetchUserInfo']);
         $router->put('/update-user-info', [AccountController::class, 'updateUserInfo']);
-
     }
 );
 
